d50b7d3dcbe840c21fd7b67682896d62107e5308
Vadim Korovin <smartdroidyt@gmail.com>
2021-05-01 22:27:06 +0300

    Папка content

---
+++ assets/img/cards/index.php
@@ -0,0 +1,119 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Catbin | каталог мини-приложеий</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://webuildthemes.com/guidebook/assets/css/vendor.css">
+        <link rel="stylesheet" href="https://webuildthemes.com/guidebook/assets/css/style.css">
+        <link rel="stylesheet" href="/assets/fonts/stylesheet.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/css/uikit.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit-icons.min.js"></script>
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+        <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+        <style>
+            body { background-color: #222226; font-family: Google Sans; }
+            @font-face {
+                font-family: 'Google Sans';
+                src: url('/assets/fonts/GoogleSans-Medium.eot');
+                src: local('Google Sans Medium'), local('GoogleSans-Medium'),
+                    url('/assets/fonts/GoogleSans-Medium.eot?#iefix') format('embedded-opentype'),
+                    url('/assets/fonts/GoogleSans-Medium.woff2') format('woff2'),
+                    url('/assets/fonts/GoogleSans-Medium.woff') format('woff'),
+                    url('/assets/fonts/GoogleSans-Medium.ttf') format('truetype');
+                font-weight: 500;
+                font-style: normal;
+                            }
+            .background-accordion {
+                background-color: aliceblue;
+            }
+
+            .uk-accordion {
+                background-color: aliceblue;
+            }
+
+            .catbin-descp {
+                margin: 64px;
+            }
+
+            a, li, h1, h2, h3, h4, h5, h6, p {
+                color: aliceblue;
+            }
+
+
+        </style>
+        <meta charset="UTF-8">
+        <meta name=viewport content="initial-scale=1, minimum-scale=1, width=device-width">
+    </head>
+<body data-spy="scroll" data-target="#toc">
+    <header class="header header-sticky header-transparent" >
+        <div class="container">
+            <div class="row">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <a class="navbar-brand" href="index.html">
+                    <img src="/assets/img/catbintext.png" width="80" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="navbar-nav ml-auto">
+                        <li class="nav-item active"></li>
+                            <a style="" class="nav-item nav-link dropdown-toggledropdown-toggle active" href="#" id="dropdown-1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Главная</a>
+                            <a style="" class="nav-item nav-link" href="https://vk.com/catb1n">Группа ВК</a>
+                            <a style="" class="nav-item nav-link" href="login.php">Каталог приложений</a>
+                            <a style="" class="nav-item nav-link" href="support.html">Помощь</a>
+                </div>
+              </nav>
+            </div>
+        </div>
+    </header>
+    <br><br><br><br><center><h1 style="font-size: 25px; color: aliceblue; font-family: Google Sans;">Каталог мини-приложений.</h1></center><br>
+    <center><h1 style="font-size: 50px; color: aliceblue; font-family: Google Sans;">Только для тебя.</h1></center>
+    <br><br>
+    <center><div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/assets/img/" class="d-block w-50" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/cards/card2-wide.jpg" class="d-block w-50" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/cards/card3-wide.jpg" class="d-block w-50" alt="...">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div></center>
+      <br><br>
+      <center><h1 style="font-size: 50px;">Почему именно Catbin?</h1></center>
+      <h3 class="catbin-descp">Catbin никогда не подведёт вас в скорости, даже при огромной нагрузке на сайт. Вы сможете получить доступ к мини-приложениям всегда и везде.</h3>
+      <br>
+      <h3 class="catbin-descp">В Catbin вы можете заходить с разных устройств, с любого места. Catbin уже совсем скоро будет поддерживать вход через: Google, ВКонтакте, Facebook и т.д.</h3>
+      <br>
+      <h3 class="cattbin-descp">У вас осталось очень мало места на диске, но там хранятся важные данные которые нельзя удалить, но под рукой у вас нету телефона, а устанавливать сотни приложений не хочется? Мы вас поняли. Просто зайдите на Catbin, зарегистрируйтесь, и уже прямо сейчас получите каталог к миниприложениям, которые не будут занимать драгоценное место на диске.</h3>
+      <br><br>
+      <center><h1>Чего же вы ждёте? Регистрируйтесь прямо сейчас!</h1></center>
+      <br><br><center><button type="button" style="background-color: darkslateblue;" name="button-reg" class="btn btn-raised btn-lg btn-primary">Зарегистрироваться</button></center>
+      <br>
+<center>
+<hr>
+<footer><font color="white">Catbin © 2020 | На основе сетях <a href="https://yunihost.ru">Юни.хостинга</a></font></footer></center><br>
+
+</body>
+</html>
